bac0e7efcc5610bc89db2620d634b07f76e8a989
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-22 12:02:03 +0300

    add pages

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,9 +8,11 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 import { useSelector } from "react-redux";
+import useAuthCalls from "../hooks/useAuthCalls";
 
 function Dashboard() {
   const { currentUser } = useSelector((state) => state.auth);
+  const { logout } = useAuthCalls();
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -20,7 +22,11 @@ function Dashboard() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             STOCK APP
           </Typography>
-          {currentUser && <Button color="inherit">Logout</Button>}
+          {currentUser && (
+            <Button color="inherit" onClick={() => logout()}>
+              Logout
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRouter from "./PrivateRouter";
