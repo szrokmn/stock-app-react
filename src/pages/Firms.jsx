fbc50b6ea7944ee1707f16af810c9a35301c8d8c
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-23 12:11:18 +0300

    add getFirms function

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,4 +1,21 @@
+import axios from "axios";
+import { useEffect } from "react";
+
+
 const Firms = () => {
+
+  const BASE_URL = "https://14164.fullstack.clarusway.com/";
+  const getFirms = async () => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}stock/firms`);
+    } catch (error) {
+      
+    }
+  }
+useEffect(() => {
+  getFirms();
+}, []);
+
   return <div>Firms</div>;
 };
 
