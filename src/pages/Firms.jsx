59703628dc956612a10ab40a79145dad00456f37
Sezer Ökmen <szrokmn34@gmail.com>
2022-11-24 09:40:49 +0300

    add axiosInstance.js

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,17 +1,22 @@
 import axios from "axios";
 import { useEffect } from "react";
+import { useSelector } from "react-redux"
 
 
 const Firms = () => {
 
+  const { token } = useSelector((state) => state.auth);
   const BASE_URL = "https://14164.fullstack.clarusway.com/";
   const getFirms = async () => {
     try {
-      const { data } = await axios.get(`${BASE_URL}stock/firms`);
+      const { data } = await axios.get(`${BASE_URL}stock/firms`, {
+      headers: { Authorization:`Token ${token}` },
+      });
     } catch (error) {
-      
+      console.log(error)
     }
   }
+  
 useEffect(() => {
   getFirms();
 }, []);
